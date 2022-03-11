55b0ef68c7f9d3aaa72646abea581c75c32d66e1
Gustavo Sant'Anna <85768828+Warywise@users.noreply.github.com>
2022-03-11 01:37:39 -0300

    Update Orders.ts

--- src/models/Orders.ts
+++ src/models/Orders.ts
@@ -4,19 +4,19 @@ import connection from './connection';
 import { GetAllReturn, OrderReturn, ProductId } from '../interfaces/ordersInterface';
 
 const GetAll = async (): Promise<GetAllReturn[]> => {
-  const [data] = await connection.execute('SELECT * FROM Trybesmith.Orders');
+  const [data] = await connection.execute('SELECT * FROM MySchema.Orders');
   return data as GetAllReturn[];
 };
 
 const Create = async (products: number[], userId: number): Promise<OrderReturn> => {
   const [getOrder] = await connection.execute<ResultSetHeader>(
-    'INSERT INTO Trybesmith.Orders (userId) VALUES (?)',
+    'INSERT INTO MySchema.Orders (userId) VALUES (?)',
     [userId],
   );
   const { insertId: orderId } = getOrder;
 
   const queryValues = products.map((prodNum) => (connection
-    .execute(`UPDATE Trybesmith.Products SET orderId = ${orderId} WHERE id = ${prodNum}`)
+    .execute(`UPDATE MySchema.Products SET orderId = ${orderId} WHERE id = ${prodNum}`)
   ));
 
   await Promise.all(queryValues);
@@ -26,7 +26,7 @@ const Create = async (products: number[], userId: number): Promise<OrderReturn> 
 
 const GetById = async (id: number): Promise<ProductId[]> => {
   const [order] = await connection
-    .execute<ProductId[]>(`SELECT id FROM Trybesmith.Products WHERE orderId=${id}`);
+    .execute<ProductId[]>(`SELECT id FROM MySchema.Products WHERE orderId=${id}`);
 
   return order;
 };
