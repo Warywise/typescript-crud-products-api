3afe2d0d171b34d36a90965a3c3b49b93fb29691
Gustavo Sant'Anna <85768828+Warywise@users.noreply.github.com>
2022-03-11 01:37:06 -0300

    Update Products.ts

--- src/models/Products.ts
+++ src/models/Products.ts
@@ -4,13 +4,13 @@ import connection from './connection';
 import { Item, ItemData, ItemFields } from '../interfaces/productInterface';
 
 const GetAll = async (): Promise<ItemData[]> => {
-  const [data] = await connection.execute('SELECT * FROM Trybesmith.Products');
+  const [data] = await connection.execute('SELECT * FROM MySchema.Products');
   return data as ItemData[];
 };
 
 const Create = async ({ name, amount }: ItemFields): Promise<Item> => {
   const [createResult] = await connection.execute<ResultSetHeader>(
-    'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+    'INSERT INTO MySchema.Products (name, amount) VALUES (?, ?)',
     [name, amount],
   );
   const { insertId: id } = createResult;
